ea70148d9123341dcddd1449fccb2b952f818959
Alex Shabalin <shabalin.media@gmail.com>
2022-03-30 00:21:47 +0600

    Added loop function an giving result function. Now the program works.

--- README.md
+++ README.md
@@ -1,1 +1,9 @@
 # rock_paper_scissors
+
+// Simple program “rock paper scissors”
+
+I made a simple implementation of grade-school classic “rock paper scissors”.
+
+It is my first program made by JavaScript.
+
+For the moment the game works from the browser console, but I will revisit this project in a later lesson and add a Graphical User Interface with buttons and text.
--- script.js
+++ script.js
@@ -5,34 +5,62 @@ function computerPlay() {
     return randomElement
 }
 
+
 // One round function with conditions of winning or loosing.
 function playRound(playerSelection, computerSelection) {
     if (playerSelection === computerSelection) {
         return "No one wins.";
     } else if (playerSelection === "rock" && computerSelection === "paper") {
-        return "Computer wins."
+        computerScore += 1;
+        return "Computer wins."    
     } else if (playerSelection === "rock" && computerSelection === "scissors") {
+        userScore +=1;
         return "Player wins."
     } else if (playerSelection === "paper" && computerSelection === "rock") {
+        userScore +=1;
         return "Player wins."
     } else if (playerSelection === "paper" && computerSelection === "scissors") {
-        return "Computer wins."
+        computerScore += 1;
+        return "Computer wins."   
     } else if (playerSelection === "scissors" && computerSelection === "paper") {
+        userScore +=1;
         return "Player wins."
     } else if (playerSelection === "scissors" && computerSelection === "rock") {
-        return "Computer wins."
+        computerScore += 1;
+        return "Computer wins."  
     }
 }
 
-// Looping playRound() five times and reports a winner or looser at the end.
+
+// Looping playRound() five times.
 function game() {
-    for (i = 0; i < 5; i++) {
-        i = playRound(playerSelection, computerSelection);
-    }
-
+    while (commonScore < 5) {
+        const playerSelection = prompt("Type rock, paper or scissors: ").toLowerCase();
+        const computerSelection = computerPlay();
+        console.log(playRound(playerSelection, computerSelection));
+        commonScore++
+        console.log(computerScore);
+        console.log(userScore);
+    } 
 }
 
 
-let playerSelection = prompt("Type rock, paper or scissors: ").toLowerCase();
-const computerSelection = computerPlay();
-console.log(game())
+// Reports a winner or looser at the end.
+function result() {
+    game();
+    if (computerScore > userScore) {
+        console.log("Computer won!")
+    } else if (computerScore < userScore) {
+        console.log("Great! You won!")
+    } else if (computerScore === userScore) {
+        console.log("Nobody won...")
+    }
+}
+
+let computerScore = 0;
+let userScore = 0;
+let commonScore = computerScore + userScore;
+
+result()
+
+
