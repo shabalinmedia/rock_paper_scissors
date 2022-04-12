21de38f55f78c6da019b6bc5ef12362859b7c3ca
Alex Shabalin <shabalin.media@gmail.com>
2022-04-12 21:36:46 +0600

    The first working version with using DOM.

--- script.js
+++ script.js
@@ -1,66 +1,66 @@
+const buttons = document.querySelectorAll('button');
+let computerScore = 0;
+let userScore = 0;
+// Click to buttons returns "rock", "paper" or "scissors" and runs playRound().
+let playerSelection = buttons.forEach((button) => {
+    button.addEventListener('click', () => {
+        playRound(button.id);
+    });
+});
+
+
 // Computer randomly returns 'rock', 'paper' or 'scissors'.
 function computerPlay() {
     let rockPaperScissors = ['rock', 'paper', 'scissors'];
     let randomElement = rockPaperScissors[Math.floor(Math.random()*rockPaperScissors.length)];
-    return randomElement
+    return randomElement;
 }
 
 
 // One round function with conditions of winning or loosing.
-function playRound(playerSelection, computerSelection) {
+function playRound(playerSelection) {
+    const computerSelection = computerPlay();
+    console.log = function(message) {
+        document.getElementById('status').innerHTML = message;
+    };
+
+    document.getElementById('userScore').innerHTML = `Your score is: ${userScore}`;
+    document.getElementById('computerScore').innerHTML = `Computer score is: ${computerScore}`;
+    game();
+    
     if (playerSelection === computerSelection) {
-        return "No one wins.";
+        console.log("No one wins.")
     } else if (playerSelection === "rock" && computerSelection === "paper") {
         computerScore += 1;
-        return "Computer wins."    
+        console.log("Computer wins.")   
     } else if (playerSelection === "rock" && computerSelection === "scissors") {
         userScore +=1;
-        return "Player wins."
+        console.log("Player wins.")
     } else if (playerSelection === "paper" && computerSelection === "rock") {
         userScore +=1;
-        return "Player wins."
+        console.log("Player wins.")
     } else if (playerSelection === "paper" && computerSelection === "scissors") {
         computerScore += 1;
-        return "Computer wins."   
+        console.log("Computer wins.")
     } else if (playerSelection === "scissors" && computerSelection === "paper") {
         userScore +=1;
-        return "Player wins."
+        console.log("Player wins.")
     } else if (playerSelection === "scissors" && computerSelection === "rock") {
         computerScore += 1;
-        return "Computer wins."  
-    }
+        console.log("Computer wins.") 
+    }    
 }
 
-
-// Looping playRound() five times.
 function game() {
-    while (commonScore < 5) {
-        const playerSelection = prompt("Type rock, paper or scissors: ").toLowerCase();
-        const computerSelection = computerPlay();
-        console.log(playRound(playerSelection, computerSelection));
-        commonScore++
-        console.log(computerScore);
-        console.log(userScore);
-    } 
-}
-
-
-// Reports a winner or looser at the end.
-function result() {
-    game();
-    if (computerScore > userScore) {
-        console.log("Computer won!")
-    } else if (computerScore < userScore) {
-        console.log("Great! You won!")
-    } else if (computerScore === userScore) {
-        console.log("Nobody won...")
+    if (userScore === 5) {
+        alert("You Won!");
+        window.location.reload();
+    }
+    else if (computerScore === 5) {
+        alert("You Lost!");
+        window.location.reload();
     }
 }
 
-let computerScore = 0;
-let userScore = 0;
-let commonScore = computerScore + userScore;
-
-result()
 
 
